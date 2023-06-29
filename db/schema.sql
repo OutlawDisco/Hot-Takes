27a82a766158b3f451371940d923ba5505ae276c
ctowns5 <128754073+ctowns5@users.noreply.github.com>
2023-06-28 21:23:49 -0600

    added initial file structure and some starter files, including a favicon setup. none of this needs to stay this way but its a place to start

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS hottakes_db;
+
+CREATE DATABASE hottakes_db;
---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: process.env.SECRET ,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
