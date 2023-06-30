9b848ce02dcbfd9a100e90521ed6b3eed8780f48
OutlawDisco <aoutlaw00@gmail.com>
2023-06-30 13:44:26 -0600

    added a user-routes.js to conrtollers, edited server.js, rearranged seed

--- seeds/index.js
+++ seeds/index.js
@@ -1,16 +1,16 @@
-const sequelize = require("../config/connection");
 const seedUsers = require("./user-seeds");
 const seedReviews = require("./review-seeds");
 const seedMovies = require("./movie-seeds");
+const sequelize = require("../config/connection");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedUsers();
 
-  await seedReviews();
-
   await seedMovies();
+
+  await seedReviews();
 
   process.exit(0);
 };
