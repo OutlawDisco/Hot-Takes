df3705507f80e18da362f615ea2b62fec95ffbd0
OutlawDisco <aoutlaw00@gmail.com>
2023-06-30 14:09:03 -0600

    fixed routers in index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const movieRouter = require('./movie');
-
-const reviewRouter = require('./review');
-
+const movieRouter = require("./movie");
+const userRouter = require("./user-routes");
+const reviewRouter = require("./review");
 
 //  will eventually store paths to user routes -- movie routes and all that jazz
-router.use('/movie', movieRouter);
-router.use('/review', reviewRouter);
+router.use("/movie", movieRouter);
+router.use("/review", reviewRouter);
+router.use("/user", userRouter);
 
 module.exports = router;
