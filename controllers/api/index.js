54994d1aca839dbf4eb3fda2995106e89343567f
Derick Stadler <derickstadler@gmail.com>
2023-06-30 10:29:39 -0600

    successful route with movie data pulled from omdb api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-
+const movieRouter = require('./movie');
 
 //  will eventually store paths to user routes -- movie routes and all that jazz
-
+router.use('/movie', movieRouter);
 
 module.exports = router;
