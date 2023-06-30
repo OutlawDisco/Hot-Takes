1ad5981e09b61ad97f3ecda062ca26eb3f11e6b2
Derick Stadler <derickstadler@gmail.com>
2023-06-30 14:15:50 -0600

    commit changes before pulling over

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const movieRouter = require('./movie');
+const userRouter = require('./user-routes')
 
 const reviewRouter = require('./review');
 
@@ -7,5 +8,6 @@ const reviewRouter = require('./review');
 //  will eventually store paths to user routes -- movie routes and all that jazz
 router.use('/movie', movieRouter);
 router.use('/review', reviewRouter);
+router.use('./user', userRouter)
 
 module.exports = router;
