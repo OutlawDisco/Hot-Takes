d8bf1399a9364c2e1a547745f169772b2d487cc4
Derick Stadler <derickstadler@gmail.com>
2023-06-30 12:35:31 -0600

    added movie-seeds content

---
+++ seeds/movie-seeds.js
@@ -0,0 +1,14 @@
+const { Movie } = require("../models");
+
+const movieData = [
+  {
+    title: "Ghostbusters",
+    plot: "new york city ghost hunters",
+  },
+  {
+    title: "Ice Age",
+    plot: "the best animated movie ever, should have won the grammy for best picture",
+  },
+];
+
+const seedMovies = () => Movie.bulkCreate(movieData);
