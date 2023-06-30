afcdde99b2a8a023828993d0a21227dad8ae342f
ctowns5 <128754073+ctowns5@users.noreply.github.com>
2023-06-30 11:12:42 -0600

    handlebars login

--- controllers/index.js
+++ controllers/index.js
@@ -5,5 +5,6 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 router.get("/",(req,res) => res.render("homepage"))
+router.get("/login",(req,res) => res.render("login"))
 
 module.exports = router;
