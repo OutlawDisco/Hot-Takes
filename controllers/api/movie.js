af9a8601f2c3ca327f0c58ba6adcc647bf54a920
Derick Stadler <derickstadler@gmail.com>
2023-07-02 18:26:51 -0600

    working movie route to attach reviews that go along

--- controllers/api/movie.js
+++ controllers/api/movie.js
@@ -1,34 +1,71 @@
 const router = require("express").Router();
-const axios = require('axios')
+const axios = require("axios");
+const { Review, Movie } = require("../../models");
 
 // /api/movie
-router.post('/', async (req, res) => {
-    try {
-        const response = await axios.get(`http://www.omdbapi.com/?t=${req.body.title}&apikey=${process.env.MOVIE_API}`)
-        // console.log('-------------');
-        // console.log(response);
-        // console.log('-------------');
+router.post("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `http://www.omdbapi.com/?t=${req.body.title}&apikey=${process.env.MOVIE_API}`
+    );
+    // console.log('-------------');
+    // console.log(response);
+    // console.log('-------------');
 
-//check against database with imdbID, if exists get all reviews
+    //check against database with imdbID, if exists get all reviews
+    // let movieExists = await Movie.findOne({
+    //     where: {
+    //         imdbID: response.data.imdbID,
+    //     }
+    // })
+    // let dbreviews;
+    // if (movieExists) {
+    //     dbreviews = await Review.findAll({
+    //         where: {
+    //             movie_id: movieExists.id,
+    //         },
+    //     })
+    // }
+    // console.log(dbreviews)
+    // res.render("moviepage", reviews);
+    const reviews = dbreviews.get({ plain: true});
 
-const data = {
-    poster: response.data.Poster,
-    title: response.data.Title,
-    plot: response.data.Plot,
-    year: response.data.Year,
-    imdbID: response.data.imdbID
-}
-for (const rating of response.data.Ratings) {
-    data[rating.Source.split(" ")[0]] = rating
-    
-}
-        // res.status(200).json(response.data);
-        res.render("moviepage", data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
+    const data = {
+      poster: response.data.Poster,
+      title: response.data.Title,
+      plot: response.data.Plot,
+      year: response.data.Year,
+      imdbID: response.data.imdbID,
+    };
+    for (const rating of response.data.Ratings) {
+      data[rating.Source.split(" ")[0]] = rating;
     }
-})
+    // res.status(200).json(response.data);
+    res.render("moviepage", data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+
+// try getting the reviews that go with a movie inside a get call with same endpoint
+
+// let movieExists = await Movie.findOne({
+//   where: {
+//       imdbID: response.data.imdbID,
+//   }
+// })
+// let dbreviews;
+// if (movieExists) {
+//   dbreviews = await Review.findAll({
+//       where: {
+//           movie_id: movieExists.id,
+//       },
+//   })
+// }
+// console.log(dbreviews)
+
 
 
 module.exports = router;
