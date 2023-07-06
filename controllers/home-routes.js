09c6cecaab2fa207ff5245e386832e2e88de721a
Derick Stadler <derickstadler@gmail.com>
2023-07-05 19:27:16 -0600

    commit

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
+const movieTitles = [
+  "Ghostbusters",
+  "Milo and Otis",
+  "Aliens",
+  "Bullet Train",
+  "Crouching Tiger Hidden Dragon"
+];
 
 router.get("/", async (req, res) => {
   try {
+    let movieInfo = [];
+    for (const movie of movieTitles) {
+      const response = await axios.get(
+        `http://www.omdbapi.com/?t=${movie}&apikey=${process.env.MOVIE_API}`
+      );
+  movieInfo.push({
+    poster: response.data.Poster,
+    imdbID: response.data.imdbID,
+  })
+    }
     res.render("homepage", {
       loggedIn: req.session.loggedIn,
+      movieInfo,
     });
   } catch (err) {
     res.status(500).json(err);
