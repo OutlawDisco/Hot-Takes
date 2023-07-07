de634d1e75021414ba66cdb5a4e07abf76a41ab7
Derick Stadler <derickstadler@gmail.com>
2023-07-07 09:27:39 -0600

    changed redirect for failed api request

--- controllers/api/movie.js
+++ controllers/api/movie.js
@@ -104,7 +104,8 @@ router.get("/", async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-    res.status(500).json(error);
+    // res.status(500).json(error);
+    res.redirect('/')
   }
 });
 
